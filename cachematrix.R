fe6cb9ce03f9b41e9c30a1401e0d563c9042e3ed
phw <phwaigel@hotmail.de>
2021-04-18 18:53:53 +0200

    pushing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+##makeCacheMatrix loads a matrix and has the sub-functions setinv, getinv, set and get
+##setinv saves a inverted matrix in caches (no caculation)
+##getinv takes the displays the inverted function (if available)
+##set can be used to change the initally loaded matrix
+##get shows the currently loaded matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  y <- NULL
+  setinv <- function(inv) y <<- inv
+  getinv <- function() y
+  set <- function(i){
+    y <<- NULL
+    x <<- i
+  }
+  get <- function() x
+  list(setinv = setinv, getinv = getinv, get = get, set = set)
+    }
 
+##cacheSolve loads a function, in this case the makeCahceMatrix function
+##it checks if there is a inverted matrix saved (through getinv function), if so, the inverted function is displayed
+##if not the inverted function is calculated, saved (through setinv function) and returned
+    
+cacheSolve <- function(x, ...){
+  y <- x$getinv()
+  if(!is.null(y)){
+    message("getting cached data")
+    return(y)
+  }
+  data <- x$get()
+  y <- solve(data)
+  x$setinv(y)
+  return(y)
 }
 
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+m2 <- matrix(c(1/2, -1/4, -1, 3/4, 4, 7, 0, 17, 1), nrow = 3, ncol = 3)
 
-## Write a short comment describing this function
+## those are my test cases
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+test <- makeCacheMatrix(m1) 
+test$get()
+test$getinv()      ##no Invertd Matrix has been calculated yet, therefore no answer expected
+test$set(m1)
+cacheSolve(test)
+cacheSolve(test)   ##second time cacheSolve to see if its calculated or taken out of the "cache" therefore the message "getting cached data"
+test$getinv()      ##as Inverted Matrix was calcualted and saved with function cacheSolve, now here is an answer expected
+test$set(m2)       ##Loading new Matrix M2
+cacheSolve(test)   ##Same Game as above just with M2
+test$getinv        ##Same Game as above just with M2
